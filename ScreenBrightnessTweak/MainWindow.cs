208c65dea6a2c0a6ed327cad4916e26212205d6c
yli <jasonleehust@gmail.com>
2018-01-07 14:24:59 -0800

    Momo implementation based on C#

---
+++ ScreenBrightnessTweak/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+  public MainWindow()
+    : base(Gtk.WindowType.Toplevel)
+  {
+    Build();
+  }
+
+  protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+  {
+    Application.Quit();
+    a.RetVal = true;
+  }
+
+  private void OnTweakEvent(object sender, EventArgs args)
+  {
+    Console.WriteLine("Brightness will be adjusted to {0}...", brightnessRange.Value / 100.0);
+    adjustBrightness(brightnessRange.Value / 100.0);
+  }
+
+  private void adjustBrightness(double brightness)
+  {
+      System.Diagnostics.Process proc = new System.Diagnostics.Process();
+      proc.EnableRaisingEvents = false; 
+      proc.StartInfo.FileName = "xrandr";
+      proc.StartInfo.Arguments = String.Format("--output eDP1 --brightness {0}", brightness);
+      proc.Start();
+      proc.WaitForExit();
+  }
+}
